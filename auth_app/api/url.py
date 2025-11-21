ad424c30249c13d2c35c6b1dd5452996863cf187
Damian Poloczek <damianpoloczek@gmail.com>
2025-11-21 22:12:14 +0100

    CookieTokenRefreshView

--- auth_app/api/url.py
+++ auth_app/api/url.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from views import CookieTokenObtainPairView 
+from views import CookieTokenObtainPairView, CookieTokenRefreshView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
 urlpatterns = [
     path('token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
 ]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from serializer import RegistrationSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
@@ -47,4 +47,36 @@ class CookieTokenObtainPairView(TokenObtainPairView):
 
         response.data = {'message': 'Du bist angemeldet'}
 
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get("refresh_token")
         
+        if refresh_token is None:
+            return Response(
+                {'message': 'Refresh token not found'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        
+        serializer = self.get_serializer(data={'refresh': refresh_token})
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except:
+            return Response(
+                {'message': 'Refresh token invalid'},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        
+        access_token = serializer.validated_data.get("access")
+
+        response = Response({'message':'Access Token refreshed'})
+
+        response.set_cookie(
+            key='access_token',
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite="Lax"
+        )
+
+        return response
